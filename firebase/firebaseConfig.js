8e201344f5ecba48e1fc0152f7a6d4073735cb86
Benjamin Desole <69907169+BenGex31@users.noreply.github.com>
2021-06-03 22:40:55 +0200

    Create firebase config

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase"
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDUVLty2LnxZv8Jki5xUYguyqRhL50szJM",
+  authDomain: "mariage-3b990.firebaseapp.com",
+  projectId: "mariage-3b990",
+  storageBucket: "mariage-3b990.appspot.com",
+  messagingSenderId: "369612524064",
+  appId: "1:369612524064:web:6ac646e73245685691ca45",
+  measurementId: "G-MNQQ5EYTXS"
+};
+
+firebase.initializeApp(firebaseConfig);
