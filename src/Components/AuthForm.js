cc1b209a19255042f85433111787130b45df4c6a
Benjamin Desole <69907169+BenGex31@users.noreply.github.com>
2021-06-06 15:14:44 +0200

    Install react-router-dom / Update css and AuthForm

--- src/Components/AuthForm.js
+++ src/Components/AuthForm.js
@@ -19,19 +19,35 @@ export default function App() {
       <h1>Connectez-vous</h1>
       <div className="signup_social">
         <button className="signup_google">
-          <img src="https://miro.com/static/images/signup/v2/google-g-logo-3.svg?cbh=7b31665a03d5b1c8a3d3eea5418e7519" alt="Google"/>
+          <img
+            src="https://miro.com/static/images/signup/v2/google-g-logo-3.svg?cbh=7b31665a03d5b1c8a3d3eea5418e7519"
+            alt="Google"
+          />
           Se connecter avec Google
         </button>
         <button className="signup_facebook">
-          <img src="https://miro.com/static/images/signup/v2/f-icon.svg?cbh=8730bf0aa763e55adbdfb6fa54a86b28" alt="Facebook" />
+          <img
+            src="https://miro.com/static/images/signup/v2/f-icon.svg?cbh=8730bf0aa763e55adbdfb6fa54a86b28"
+            alt="Facebook"
+          />
           Se connecter avec facebook
         </button>
       </div>
       {/* register your input into the hook by invoking the "register" function */}
-      <input className="inputAuth" placeholder="Adresse mail personnel" {...register("example")} />
+      <input
+        type="text"
+        className="inputAuth"
+        placeholder="Adresse mail personnel"
+        {...register("example")}
+      />
 
       {/* include validation with required or other standard HTML validation rules */}
-      <input className="inputAuth" placeholder="Mot de passe" {...register("exampleRequired", { required: true })} />
+      <input
+        type="text"
+        className="inputAuth"
+        placeholder="Mot de passe"
+        {...register("exampleRequired", { required: true })}
+      />
       {/* errors will return when field validation fails  */}
       {errors.exampleRequired && <span>This field is required</span>}
 
